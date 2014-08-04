35a749c47e4fa9f6430989a99e31de14f0b8139f
Cookeh007 <zach@MAC-IMAGE-050310.local>
2014-08-04 14:34:30 -0400

    part 2 of whatever i just did….

--- js/script.js
+++ js/script.js
@@ -4,7 +4,9 @@ $(document).ready(function(){
   $("img").show(6000);
   $("h1").hide(2000);
   $("p").hide(2000);
+  $("p").show(2000);
   $("ul").hide(2000);
+  $("ul").show(2000);
   //$("h1").text("bean");
   $("body").hide(6000).show(7000);
   //$("body").show(7000);
