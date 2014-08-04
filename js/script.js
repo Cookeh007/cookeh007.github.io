b078bee5a790f625499a32b2e4a8834658176290
Cookeh007 <zach@MAC-IMAGE-050310.local>
2014-08-04 14:31:44 -0400

    woosh bye bye

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// Code goes here
+$(document).ready(function(){
+  $("img").hide(6000);
+  $("img").show(6000);
+  $("h1").hide(2000);
+  $("p").hide(2000);
+  $("ul").hide(2000);
+  //$("h1").text("bean");
+  $("body").hide(6000).show(7000);
+  //$("body").show(7000);
+});
+
